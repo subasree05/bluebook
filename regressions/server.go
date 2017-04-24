3feb4942a2a486da7f3ad8b2a07c5df800fa87ae
Tadas Vilkeliskis <vilkeliskis.t@gmail.com>
2017-04-23 21:24:52 -0400

    add new regressions for request body

--- regressions/server.go
+++ regressions/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -10,8 +11,20 @@ func JsonResponseHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "{\"data\":[\"string\"]}")
 }
 
+func EchoHandler(w http.ResponseWriter, req *http.Request) {
+	defer req.Body.Close()
+	data, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		http.Error(w, "unable to read body", 400)
+		return
+	}
+
+	w.Write(data)
+}
+
 func main() {
 	http.HandleFunc("/404", http.NotFound)
 	http.HandleFunc("/json-response", JsonResponseHandler)
+	http.HandleFunc("/echo-body", EchoHandler)
 	log.Fatal(http.ListenAndServe(":12345", nil))
 }
