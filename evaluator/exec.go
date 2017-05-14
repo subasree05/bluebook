54c54158b7c7040c8484424ef90a23cd59136a9d
Tadas Vilkeliskis <vilkeliskis.t@gmail.com>
2017-05-13 22:44:07 -0400

    global variables

--- evaluator/exec.go
+++ evaluator/exec.go
@@ -22,6 +22,8 @@ var resourceFactoryTable = map[string]resource.FactoryFunc{
 	"system_variable": system_variable.New,
 }
 
+var globalVariables = map[string]string{}
+
 type evaluatorState struct {
 	refToResourceMap map[string]resource.Resource
 	idToResourceMap  map[string]resource.Resource
@@ -35,8 +37,9 @@ func initializeDrivers(tree *bcl.Tree, executionContext *resource.ExecutionConte
 		}
 
 		nodeBlock := node.(*bcl.BlockNode)
+		blockId := string(nodeBlock.Id.Text)
 
-		if string(nodeBlock.Id.Text) == "resource" {
+		if blockId == "resource" {
 			if factory, ok := resourceFactoryTable[string(nodeBlock.Driver.Text)]; ok {
 				d, err := factory(nodeBlock)
 				if err != nil {
@@ -50,6 +53,21 @@ func initializeDrivers(tree *bcl.Tree, executionContext *resource.ExecutionConte
 			} else {
 				return fmt.Errorf("unsupported resource: %s", nodeBlock.Driver)
 			}
+		} else if blockId == "variable" {
+			variableName := string(nodeBlock.Name.Text)
+			variableValue := ""
+			for _, expression := range nodeBlock.Expressions {
+				if string(expression.Field.Text) == "default" {
+					variableValue = string(expression.Value.(*bcl.StringNode).Text)
+					break
+				}
+			}
+
+			if len(variableValue) == 0 {
+				return fmt.Errorf("variable %s is missing default value", variableName)
+			}
+
+			globalVariables[variableName] = variableValue
 		} else {
 			return fmt.Errorf("unsupported block type: %s", nodeBlock.Id.Text)
 		}
@@ -88,6 +106,11 @@ func Exec(tree *bcl.Tree) error {
 
 	for ref, r := range executionContext.ReferenceToResourceMap {
 		if strings.HasPrefix(ref, "http_test.") {
+			// TODO reset context
+			for variable, value := range globalVariables {
+				executionContext.SetVariable(variable, value)
+			}
+
 			err := r.Exec(executionContext)
 			if err != nil {
 				numFailedTests++
